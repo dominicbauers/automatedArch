8bc892501b05d88a5a82569c29afdde1e62d255b
dominicbauers <dominic@DominicsDesktop.localdomain>
2020-12-27 01:39:29 -0500

    bootstrap

--- sata.sh
+++ sata.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 echo 'sata'
 parted /dev/sda --script mklabel gpt
-parted /dev/sda --script mkpart primary esp 1MiB 512MiB
+parted /dev/sda --script mkpart primary fat32 1MiB 512MiB
 parted /dev/sda --script mkpart primary linux-swap 512MiB 8512Mib
 parted /dev/sda --script mkpart primary ext4 8512Mib 100%
+parted /dev/sda --script set 1 esp on
 
 mkfs.vfat /dev/sda1
 mkswap /dev/sda2
@@ -16,8 +17,10 @@ mount /dev/sda1 /mnt/boot
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
-pacstrap /mnt base base-devel linux linux-firmware networkmanager vim man-db man-pages
+#pacstrap /mnt --quiet base base-devel linux linux-firmware networkmanager vim man-db man-pages
 
-cp -r /postChroot /mnt
+#echo "after pacman"
 
-arch-chroot /mnt ./postChroot/postChroot.sh
+#cp -r /postChroot /mnt
+
+#arch-chroot /mnt ./postChroot/postChroot.sh
