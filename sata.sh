18e8fbba680772a064fe76ecd46e4a4b591a1862
dominicbauers <dominic@DominicsDesktop.localdomain>
2020-12-27 00:41:37 -0500

    partitioning

--- sata.sh
+++ sata.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
 echo 'sata'
+parted /dev/sda --script mklabel gpt
+parted /dev/sda --script mkpart primary vfat 1MiB 512MiB
+parted /dev/sda --script mkpart primary linux-swap 512MiB 8512Mib
+parted /dev/sda --script mkpart primary ext4 8512Mib 100%
+
