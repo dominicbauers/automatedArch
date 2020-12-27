742915811aed1f25b66645fd5d6a39fca387db5d
dominicbauers <dominic@DominicsDesktop.localdomain>
2020-12-27 06:20:09 -0500

    fixed bootstrap + script

--- postChroot/postChroot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "script2success"
-#lsblk -no UUID /dev/sda3
--- sata.sh
+++ sata.sh
@@ -38,6 +38,8 @@ bootctl --path=/boot install
 echo $'default/tarch-*' >> /boot/loader/loader.conf
 cp postChroot/arch.conf /boot/loader/entries/arch.conf
 echo $'options\troot=UUID=$UUID rw' >> /boot/loader/entries/arch.conf
+cp -R postChroot/sudoers /etc/sudoers
+cp -r postChroot/bootstrap /root 
 rm -rf postChroot
 EOF
 reboot
