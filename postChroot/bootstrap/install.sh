de93c6898e3b58b953e32285c5bca0301e4a28f5
dominicbauers <dominic@DominicsDesktop.localdomain>
2021-04-10 21:11:18 -0400

    merging scripts

---
+++ postChroot/bootstrap/install.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+echo Enter 1 for SATA, 2 for NVME, 3 for VIRTIO
+read disk
+if [ $disk = "1" ]; then
+        echo "Your disk is SATA"
+elif [ $disk = "2" ]; then
+        echo "Your disk is NVME"
+elif [ $disk = "3" ]; then
+        parted /dev/vda --script mklabel gpt
+	parted /dev/vda --script mkpart primary fat32 1MiB 512MiB
+	parted /dev/vda --script mkpart primary linux-swap 512MiB 8512Mib
+	parted /dev/vda --script mkpart primary ext4 8512Mib 100%
+	parted /dev/vda --script set 1 esp on
+
+	mkfs.vfat /dev/vda1
+	mkswap /dev/vda2
+	swapon /dev/vda2
+	mkfs.ext4 /dev/vda3
+
+	mount /dev/vda3 /mnt
+	mkdir /mnt/boot
+	mount /dev/vda1 /mnt/boot
+else
+        echo "Invalid input, try again :("
+fi
+
+pacstrap /mnt --quiet base base-devel linux linux-firmware networkmanager vim man-db man-pages grub efibootmgr
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+cp -r postChroot /mnt
+
+
+arch-chroot /mnt <<EOF
+echo 'y' | pacman -S linux
+ln -sf /usr/share/zoneinfo/America/Detroit /etc/localtime
+hwclock --systohc
+cp /postChroot/locale.gen /etc/locale.gen
+locale-gen
+echo 'LANG=en_US.UTF-8' > /etc/locale.conf
+echo 'archbox' > /etc/hostname
+cp postChroot/hosts /etc/hosts
+echo "root:password" | chpasswd
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+cp -r postChroot/sudoers /etc/sudoers
+cp -r postChroot/bootstrap /home
+rm -rf postChroot
+systemctl enable NetworkManager
+echo "root:password" | chpasswd
+useradd -m -G wheel user
+echo "user:password" | chpasswd
+pacman -S --noconfirm git plasma dolphin sddm konsole firefox
+systemctl enable sddm
+EOF
+reboot
+
